dbea60c412ac3fc26f49dd824ed2e3d13ad16f8f
Linga2106 <lingaraja2106@gmail.com>
2024-08-28 13:08:03 +0530

    first

---
+++ src/org/xpath/XpathFinder.java
@@ -0,0 +1,24 @@
+package org.xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathFinder {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "H:\\Eclipse Workspace\\DifferentXpath\\driver\\chromedriver.exe");
+		
+		WebDriver driver =new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("home");
+		
+		
+	}
+
+}
